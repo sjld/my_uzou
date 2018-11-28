66f81e8c71b506223525a52ffa8496a764165444
sjld <44466556+sjld@users.noreply.github.com>
2018-11-28 14:18:25 +0800

    Update uzou-hotel.js

--- routes/uzou-hotel.js
+++ routes/uzou-hotel.js
@@ -21,21 +21,4 @@ router.get('/u_hotel',(req,res)=>{
        res.end();
    })
 })
-/*router.get('/page',(req,res)=>{
-    var output={pageSize:5} 
-   output.pno=req.query.pno;//几条数据
-    var sql="select * from zhusu";
-    pool.query(sql,[],(err,result)=>{
-        if(err) console.log(err);
-        output.count=result.length;//总记录数
-        output.pageCount=Math.ceil(output.count/output.pageSize);//总页数
-        output.products=result.slice(output.pno*5,output.pno*5+5);
-        res.writeHead(200,{
-            "Content-Type":"application/json;charset=utf-8","Access-Control-Allow-origin":"*"
-        })
-        res.write(JSON.stringify(output))
-        res.end()
-    })
-
-})*/
 module.exports=router;
